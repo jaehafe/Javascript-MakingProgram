005d3d276db3b01327f4483c98014df4805101a8
jaehafe <wogkdkrm112@gmail.com>
2022-07-30 15:18:09 +0900

    Update async.js

--- js문법/async.js
+++ js문법/async.js
@@ -1,9 +1,19 @@
-// 성공만 가능
-
-async function 더하기(){
-  return 1 + 1;
+function timer(time) {
+  return new Promise(function(reslove) {
+    setTimeout(function(){
+      reslove(time);
+    }, time)
+  });
 }
 
-더하기().then(function(결과){
-  console.log(결과)
-})
+async function run() {
+  console.log('start');
+  var time = await timer(1000);
+  console.log('time:'+time);
+  time = await timer(time+1000);
+  console.log('time:'+time);
+  time = await timer(time+1000);
+  console.log('time:'+time);
+  console.log('end');
+}
+run();
